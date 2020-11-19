2e51103acba7c68558660a5768b52df79569717b
ashitechdev <ashitechdev@gmail.com>
2020-11-19 15:21:25 +0530

    add a text displaying current theme

--- provider_theme_changer/lib/main.dart
+++ provider_theme_changer/lib/main.dart
@@ -1,117 +1,150 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'Model/thememodel.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MaterialApp(
+      home: ChangeNotifierProvider<ThemeModel>(
+        create: (_) => ThemeModel(),
+        child: MyApp(),
+      ),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      /// theme of the whole App under this Material Widget i.e. Screen1 and Screen2
+      theme: Provider.of<ThemeModel>(context, listen: true).currentTheme,
+      home: Screen1(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+/// Screen 1
+class Screen1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Screen 1"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.lightbulb_outline,
+        ),
+        onPressed: () {
+          Provider.of<ThemeModel>(context, listen: false).changeTheme();
+        },
+        tooltip: "Change Theme",
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Container(
+              child: Text(
+                "Yoo this is a text",
+                textScaleFactor: 2.0,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SizedBox(
+              height: 30,
             ),
+
+            /// lets use Provider.of to check which theme is currently active
+            Container(
+              child:
+                  Provider.of<ThemeModel>(context, listen: true).currentTheme ==
+                          ThemeData.light()
+                      ? Text(
+                          "Light Theme is active rn",
+                          textScaleFactor: 1.8,
+                        )
+                      : Text(
+                          "Dark Theme is active rn",
+                          textScaleFactor: 1.8,
+                        ),
+            ),
+
+            SizedBox(
+              height: 30,
+            ),
+            FlatButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return Screen2();
+                  }));
+                },
+                child: Text("Go to Screen 2"))
           ],
         ),
       ),
+    );
+  }
+}
+
+///Screen 2
+class Screen2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Screen 2"),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(
+          Icons.lightbulb_outline,
+        ),
+        tooltip: "Change Theme",
+        onPressed: () {
+          Provider.of<ThemeModel>(context, listen: false).changeTheme();
+        },
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Text(
+                "Yoo this is a text",
+                textScaleFactor: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+
+            /// lets use Provider.of to check which theme is currently active
+            Container(
+              child:
+                  Provider.of<ThemeModel>(context, listen: true).currentTheme ==
+                          ThemeData.light()
+                      ? Text(
+                          "Light Theme is active rn",
+                          textScaleFactor: 1.8,
+                        )
+                      : Text(
+                          "Dark Theme is active rn",
+                          textScaleFactor: 1.8,
+                        ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text("Back to Screen 1"))
+          ],
+        ),
+      ),
     );
   }
 }
